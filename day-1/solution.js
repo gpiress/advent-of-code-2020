0c2f007be475a74ded915d5c1c68533f2ad1f622
gpires <gpires@spotify.com>
2020-12-01 14:32:19 +0100

    Second part

--- day-1/solution.js
+++ day-1/solution.js
@@ -18,7 +18,7 @@ const processLine = line => {
     values.push(value);
 }
 
-const computeResults = () => {
+const firstPart = () => {
     for (const first of values) {
         const toFind = 2020 - first;
         const second = values.find(element => (element === toFind));
@@ -28,6 +28,46 @@ const computeResults = () => {
             break;
         }
     }
+}
+
+const secondPart = () => {
+
+    for (let i = 0; i < values.length - 2; i++) {
+        let sum = values[i];
+        const first = values[i];
+        let second = 0;
+        let third = 0;
+
+        for (let j = i + 1; j < values.length - 1; j++) {
+            second = values[j];
+            sum = first + second;
+
+            if (sum >= 2020) {
+                break;
+            }
+
+            for (let k = j + 1; k < values.length; k++) {
+                third = values[k];
+                sum = first + second + third;
+
+                if (sum === 2020) {
+                    console.log(`${first} * ${second} * ${third}`);
+                    console.log(first * second * third);
+                    return;
+                }
+
+                if (sum > 2020) {
+                    break;
+                }
+            }
+        }
+    }
+}
+
+const computeResults = () => {
+    values.sort();
+    firstPart();
+    secondPart();
 }
 
 // Read from input.in
