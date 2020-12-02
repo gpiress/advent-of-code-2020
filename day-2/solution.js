c88492b95e08ebbc8f7d7be6d0a998533c88b710
gpires <gpires@spotify.com>
2020-12-02 14:47:02 +0100

    day 2: second part

--- day-2/solution.js
+++ day-2/solution.js
@@ -1,7 +1,34 @@
 const readline = require('readline');
 const fs = require('fs');
 
-let valid = 0;
+let firstPartValid = 0;
+let secondPartValid = 0;
+
+const firstPart = (first, second, character, password) => {
+    let letterCount = 0;
+    for (let c of password) {
+        if (c === character) {
+            letterCount++;
+        }
+
+        if (letterCount > second) {
+            break;
+        }
+    }
+
+    if (letterCount >= first && letterCount <= second) {
+        firstPartValid++;
+    }
+}
+
+const secondPart = (first, second, character, password) => {
+    if (password[first - 1] === character) {
+        return (password[second - 1] !== character);
+    }
+
+    return (password[second - 1] === character);
+}
+
 const processLine = line => {
     // each line is like : "3-11 k: kkqkkfkkvkgfknkx"
     const lineParts = line.split(" ");
@@ -16,25 +43,18 @@ const processLine = line => {
     // kkqkkfkkvkgfknkx
     const password = lineParts[2];
 
-    let letterCount = 0;
-    for (let c of password) {
-        if (c === letter) {
-            letterCount++;
-        }
-
-        if (letterCount > highest) {
-            break;
-        }
-    }
-
-    if (letterCount >= lowest && letterCount <= highest) {
+    // Solution
+    firstPart(lowest, highest, letter, password);
+    const isValid = secondPart(lowest, highest, letter, password);
+    if (isValid) {
         console.log(`${line} is valid`);
-        valid++;
+        secondPartValid++;
     }
 }
 
 const computeResults = () => {
-    console.log(valid);
+    console.log(`First part: ${firstPartValid}`);
+    console.log(`Second part: ${secondPartValid}`);
 }
 
 // Read from input.in
