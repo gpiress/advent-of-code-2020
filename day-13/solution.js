f08b46d56f65ffee6e849b60acc60e749fb042d0
gpiress <gpires@spotify.com>
2020-12-13 18:44:13 +0100

    Day 13: both parts

--- day-13/solution.js
+++ day-13/solution.js
@@ -39,6 +39,86 @@ const busToAirport = inputs => {
     };
 };
 
+const busesLeavingNext = inputs => {
+    const findStartTime = busesObjects => {
+        const findInverse = (n, mod) => {
+            let x = 1;
+            let temp = BigInt(n) % BigInt(mod);
+            while (temp !== BigInt(1)) {
+                temp = temp + BigInt(n);
+                temp = temp % BigInt(mod);
+                x++;
+            }
+
+            return x;
+        }
+
+        const findRemainder = (busId, offset) => {
+            if (offset <= busId) {
+                return (busId - offset) % busId;
+            }
+
+            let temp = -offset;
+            while (temp < 0) {
+                temp += busId;
+            }
+            return temp % busId;
+        }
+
+        let N = busesObjects
+            .map(busObj => busObj.id)
+            .reduce((previous, busId) => BigInt(previous) * BigInt(busId), BigInt(1));
+
+        console.log(`   N: ${N}`);
+
+        const theTime = busesObjects
+            .map(busObj => {
+                console.log(`   bus: ${busObj.id}. offset: ${busObj.offset}`);
+                const bi = findRemainder(busObj.id, busObj.offset);
+                console.log('bi calculated');
+                const ni = N / BigInt(busObj.id);
+                console.log('ni calculated');
+                const x = findInverse(ni, busObj.id);
+                console.log('xi calculated');
+                let product = BigInt(bi) * BigInt(ni) * BigInt(x);
+                product = product % N;
+                console.log('product calculated');
+
+                console.log(`   bi: ${bi}. ni: ${ni}. xi: ${x}. product: ${product}`);
+                return {
+                    b: bi,
+                    n: ni,
+                    x: x,
+                    product: product,
+                }
+            })
+            .map(chineseRemObj => chineseRemObj.product)
+            .reduce((previous, current) => (previous + current) % N, BigInt(0));
+
+        return (theTime % N);
+    };
+
+    // for inputs[1] = "7,13,x,x,59,x,31,19" --> 
+    // busIds = [{id: 7, offset: 0}, {id: 13, offset: 1}, {id: 59, offset: 4}, ...]
+    const busIds = inputs[1]
+        .split(',')
+        .map((busId, index) => {
+            return { 
+                id: busId,
+                offset: index
+            };
+        })
+        .filter(busObj => busObj.id !== 'x')
+        .map(busObj => {
+            return {
+                id: +busObj.id,
+                offset: busObj.offset
+            };
+        });
+
+    return findStartTime(busIds);
+};
+
 
 let inputs = [];
 const processLine = line => {
@@ -49,7 +129,7 @@ const computeResults = () => {
     const firstBusInfo = busToAirport(inputs);
     console.log(JSON.stringify(firstBusInfo));
     console.log(`First part: ${firstBusInfo.busId * firstBusInfo.wait}`);
-    console.log(`Second part: ${-1}`);
+    console.log(`Second part: ${busesLeavingNext(inputs)}`);
 };
 
 // Read from input.in
